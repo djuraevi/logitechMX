2b5874e2e2c66e1b21e30c2abcd9c54fd40973ed
Djuraev Iskander <djuraevi@yandex.ru>
2021-09-06 12:29:33 +0300

    add slider three-page
    add slider three-page

--- index.js
+++ index.js
@@ -16,3 +16,41 @@ if (menuBurger.classList.contains('_active')){
     menuBurger.classList.remove('_active');
     enuList.classList.remove('_active');
 }
+
+// слайдер
+
+let offset = 0;
+const sliderLine = document.querySelector('.slider-line');
+
+document.querySelector('.right').addEventListener('click', function () {
+    offset = offset + 375;
+    if (offset > 1500) {
+        offset = 0;
+    }
+    sliderLine.style.left = - offset + 'px';
+})
+
+document.querySelector('.left').addEventListener('click', function () {
+    offset = offset - 375;
+    if (offset < 0) {
+        offset = 1500;
+    }
+    sliderLine.style.left = - offset + 'px';
+})
+
+document.querySelector('.wood-texture').addEventListener('click', function () {
+    sliderLine.style.left = 0;
+})
+document.querySelector('.glass-texture').addEventListener('click', function () {
+    sliderLine.style.left = -375 + 'px';
+})
+document.querySelector('.linoleum-texture').addEventListener('click', function () {
+    sliderLine.style.left = -750 + 'px';
+})
+document.querySelector('.cloth-texture').addEventListener('click', function () {
+    sliderLine.style.left = -1125 + 'px';
+})
+document.querySelector('.marble-texture').addEventListener('click', function () {
+    sliderLine.style.left = -1500+ 'px';
+})
+
