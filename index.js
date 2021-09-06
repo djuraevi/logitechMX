617db6ba091d35b65bc0771e865922feda6d05d4
Djuraev Iskander <djuraevi@yandex.ru>
2021-09-06 16:03:29 +0300

    add slider five-page
    add slider five-page
    and centre web site

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// меню бургер
+// menu burger first-page
 
 const menuBurger = document.querySelector('.menu__burger');
 const menuList = document.querySelector('.menu__list');
@@ -12,12 +12,12 @@ if(menuBurger){
 }
 
 if (menuBurger.classList.contains('_active')){
-    document.body.classList.remove('_lock');
-    menuBurger.classList.remove('_active');
-    enuList.classList.remove('_active');
+        document.body.classList.remove('_lock');
+        menuBurger.classList.remove('_active');
+        enuList.classList.remove('_active');
 }
 
-// слайдер
+// slider three-page
 
 let offset = 0;
 const sliderLine = document.querySelector('.slider-line');
@@ -53,4 +53,28 @@ document.querySelector('.cloth-texture').addEventListener('click', function () {
 document.querySelector('.marble-texture').addEventListener('click', function () {
     sliderLine.style.left = -1500+ 'px';
 })
+
+// slider five-page
+
+const sliderMouse = document.querySelector('.slider-line__mouse');
+
+document.querySelector('.one').addEventListener('click', function () {
+    sliderMouse.style.left = 0;
+})
+document.querySelector('.two').addEventListener('click', function () {
+    sliderMouse.style.left = -330 + 'px';
+})
+document.querySelector('.three').addEventListener('click', function () {
+    sliderMouse.style.left = -660 + 'px';
+})
+document.querySelector('.four').addEventListener('click', function () {
+    sliderMouse.style.left = -990 + 'px';
+})
+document.querySelector('.five').addEventListener('click', function () {
+    sliderMouse.style.left = -1320 + 'px';
+})
+document.querySelector('.six').addEventListener('click', function () {
+    sliderMouse.style.left = -1650 + 'px';
+})
+
 
