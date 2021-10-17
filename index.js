270102b44a58bf3f6362aeea01c077a5da98491a
Djuraev Iskander <djuraevi@yandex.ru>
2021-10-17 14:21:28 +0300

    fix bag carousel
    adaptive carousel

--- index.js
+++ index.js
@@ -1,80 +1,107 @@
                 // menu burger first-page
 
-// const menuBurger = document.querySelector('.menu__burger');
-// const menuList = document.querySelector('.menu__list');
+const menuBurger = document.querySelector('.menu__burger');
+const menuList = document.querySelector('.menu__list');
 
-// if(menuBurger){
-//     menuBurger.addEventListener("click", function(e){
-//         document.body.classList.toggle('_lock');
-//         menuBurger.classList.toggle('_active');
-//         menuList.classList.toggle('_active');
-//     });
-// }
+if(menuBurger){
+    menuBurger.addEventListener("click", function(e){
+        document.body.classList.toggle('_lock');
+        menuBurger.classList.toggle('_active');
+        menuList.classList.toggle('_active');
+    });
+}
 
-// if (menuBurger.classList.contains('_active')){
-//         document.body.classList.remove('_lock');
-//         menuBurger.classList.remove('_active');
-//         enuList.classList.remove('_active');
-// }
+if (menuBurger.classList.contains('_active')){
+        document.body.classList.remove('_lock');
+        menuBurger.classList.remove('_active');
+        enuList.classList.remove('_active');
+}
 
-                // slider three-page
+                // slider three-page     
+let offset = 0;
+let widthDevice = 0;
+                
+function showDevice(){
+    if (window.innerWidth < 894) {
+        widthDevice = 375;
+    }
+    else if (window.innerWidth >= 894 == window.innerWidth < 1280) {
+        widthDevice = 894;
+    }
+    else if (window.innerWidth >= 1280) {
+        widthDevice = 1280;
+    }
+}
 
-// let offset = 0;
-// const sliderLine = document.querySelector('.slider-line');
+const sliderLine = document.querySelector('.slider-line');
 
-// document.querySelector('.right').addEventListener('click', function () {
-//     offset = offset + 375;
-//     if (offset > 1500) {
-//         offset = 0;
-//     }
-//     sliderLine.style.left = - offset + 'px';
-// })
+document.querySelector('.right').addEventListener('click', function () {
+    showDevice();
+    offset = offset + widthDevice;
+    if (offset > widthDevice * 4) {
+        offset = 0;
+    }
+    sliderLine.style.left = - offset + 'px';
+})
 
-// document.querySelector('.left').addEventListener('click', function () {
-//     offset = offset - 375;
-//     if (offset < 0) {
-//         offset = 1500;
-//     }
-//     sliderLine.style.left = - offset + 'px';
-// })
+document.querySelector('.left').addEventListener('click', function () {
+    showDevice();
+    offset = offset - widthDevice;
+    if (offset < 0) {
+        offset = widthDevice * 4;
+    }
+    sliderLine.style.left = - offset + 'px';
+})
 
-// document.querySelector('.wood-texture').addEventListener('click', function () {
-//     sliderLine.style.left = 0;
-// })
-// document.querySelector('.glass-texture').addEventListener('click', function () {
-//     sliderLine.style.left = -375 + 'px';
-// })
-// document.querySelector('.linoleum-texture').addEventListener('click', function () {
-//     sliderLine.style.left = -750 + 'px';
-// })
-// document.querySelector('.cloth-texture').addEventListener('click', function () {
-//     sliderLine.style.left = -1125 + 'px';
-// })
-// document.querySelector('.marble-texture').addEventListener('click', function () {
-//     sliderLine.style.left = -1500+ 'px';
-// })
+document.querySelector('.wood-texture').addEventListener('click', function () {
+    sliderLine.style.left = 0;
+})
+document.querySelector('.glass-texture').addEventListener('click', function () {
+    sliderLine.style.left = -widthDevice + 'px';
+})
+document.querySelector('.linoleum-texture').addEventListener('click', function () {
+    sliderLine.style.left = -(widthDevice*2) + 'px';
+})
+document.querySelector('.cloth-texture').addEventListener('click', function () {
+    sliderLine.style.left = -(widthDevice*3) + 'px';
+})
+document.querySelector('.marble-texture').addEventListener('click', function () {
+    sliderLine.style.left = -(widthDevice*4)+ 'px';
+})
 
                         // slider five-page
+let widhtDeviceTwo = 0;
 
-// const sliderMouse = document.querySelector('.slider-line__mouse');
+function showDeviceTwo(){
+    if (window.innerWidth < 894) {
+        widthDeviceTwo = 330;
+    }
+    else {
+        widthDeviceTwo = 497;
+    }
+}
 
-// document.querySelector('.one').addEventListener('click', function () {
-//     sliderMouse.style.left = 0;
-// })
-// document.querySelector('.two').addEventListener('click', function () {
-//     sliderMouse.style.left = -330 + 'px';
-// })
-// document.querySelector('.three').addEventListener('click', function () {
-//     sliderMouse.style.left = -660 + 'px';
-// })
-// document.querySelector('.four').addEventListener('click', function () {
-//     sliderMouse.style.left = -990 + 'px';
-// })
-// document.querySelector('.five').addEventListener('click', function () {
-//     sliderMouse.style.left = -1320 + 'px';
-// })
-// document.querySelector('.six').addEventListener('click', function () {
-//     sliderMouse.style.left = -1650 + 'px';
-// })
+showDeviceTwo();
+
+const sliderMouse = document.querySelector('.slider-line__mouse');
+
+document.querySelector('.one').addEventListener('click', function () {
+    sliderMouse.style.left = 0;
+})
+document.querySelector('.two').addEventListener('click', function () {
+    sliderMouse.style.left = - widthDeviceTwo + 'px';
+})
+document.querySelector('.three').addEventListener('click', function () {
+    sliderMouse.style.left = - (widthDeviceTwo*2) + 'px';
+})
+document.querySelector('.four').addEventListener('click', function () {
+    sliderMouse.style.left = - (widthDeviceTwo*3) + 'px';
+})
+document.querySelector('.five').addEventListener('click', function () {
+    sliderMouse.style.left = - (widthDeviceTwo*4) + 'px';
+})
+document.querySelector('.six').addEventListener('click', function () {
+    sliderMouse.style.left = - (widthDeviceTwo*5) + 'px';
+})
 
 
